d918841f8d2ce6fd284b4f54df8977f368531bc4
Vijayaraagavan <96820450+Vijayaraagavan@users.noreply.github.com>
2022-05-23 12:17:31 +0530

    Update with another solution

--- hamming.go
+++ hamming.go
@@ -27,3 +27,28 @@ func Distance(as, bs string) (int, error) {
 
 /*
 use errors package for generating erros of type 'error'. If range has only one LHS variable it returns index. Orelse index and value ( key, value := range a { )
+*/
+
+func Distance(strand1 string, strand2 string) (distance int, e error) {
+	if utf8.RuneCountInString(strand1) != utf8.RuneCountInString(strand2) {
+		return 0, errors.New("strands must be of equal length. they're not. no soup for you")
+	}
+	// compare runes, not bytes
+	strand1Runes := []rune(strand1)
+	strand2Runes := []rune(strand2)
+	// range iterates runes
+	// but the index from range on a string increments to the position of the first byte of the next rune in the string,
+	// not to the rune's position in a slice of runes, so we'd  need to manually increment the index ourselves.
+	// iterating on strand1Runes := []rune(strand1) returns the slice index.
+	for idx, rooney := range strand1Runes {
+		if rooney != strand2Runes[idx] {
+			distance++
+		}
+	}
+	return distance, nil
+}
+
+/*
+byte datatye represents only 127 characters (1byte). so if characters have utf8 encoding indexing will not be proper. Also len() function also returns wrong value.
+Thats why we use rune datatype. it has length of 4bytes. for example chinese letters occupy 4bytes of data(thats what its unicode is). In that case len() will
+return 4 for single character, but we expect 1. utf8.RuneCountInStrings() returns correct length
